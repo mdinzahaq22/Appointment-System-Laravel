45531b9940b5072ba65411456198a5aeccf740d9
Injamamul <injamam_haq>
2022-01-16 17:27:00 +0530

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Home Page Routes
+Route::get('/', 'FrontEndController@index');
+Route::get('/new-appointment/{doctorId}/{date}', 'FrontEndController@show')->name('create.appointment');
+
+Route::get('/dashboard', 'DashBoardController@index');
+
+Route::get('/home', 'HomeController@index');
+
+Auth::routes();
+
+// Patient Routes
+Route::group(['middleware' => ['auth', 'patient']], function () {
+    // Profile Routes
+    Route::get('/user-profile', 'ProfileController@index')->name('profile');
+    Route::post('/user-profile', 'ProfileController@store')->name('profile.store');
+    Route::post('/profile-pic', 'ProfileController@profilePic')->name('profile.pic');
+
+    Route::post('/book/appointment', 'FrontEndController@store')->name('book.appointment');
+    Route::get('/my-booking', 'FrontEndController@myBookings')->name('my.booking');
+});
+// Admin Routes
+Route::group(['middleware' => ['auth', 'admin']], function () {
+    Route::resource('doctor', 'DoctorController');
+    Route::get('/patients', 'PatientListController@index')->name('patients');
+    Route::get('/status/update/{id}', 'PatientListController@toggleStatus')->name('update.status');
+    Route::get('/all-patients', 'PatientListController@allTimeAppointment')->name('all.appointments');
+    Route::resource('/department', 'DepartmentController');
+});
+// Doctor Routes
+Route::group(['middleware' => ['auth', 'doctor']], function () {
+    Route::resource('appointment', 'AppointmentController');
+    Route::get('/appointmentsList', 'PatientListController@index')->name('patient.appointments');
+    Route::post('/appointment/check', 'AppointmentController@check')->name('appointment.check');
+    Route::post('/appointment/update', 'AppointmentController@updateTime')->name('update');
+    Route::get('patient-today', 'PrescriptionController@index')->name('patient.today');
+    Route::get('/status/update/{id}', 'PatientListController@toggleStatus')->name('update.status');
+});
